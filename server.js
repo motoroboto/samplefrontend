2b8e1f49fbc2bb79eb61deb3ece4ea814fc26fa6
motoroboto <robert.a.cullen@gmail.com>
2020-12-17 17:46:23 -0500

    newer version

---
+++ server.js
@@ -0,0 +1,18 @@
+/* eslint-disable indent */
+require('dotenv').config() // read .env files
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Set public folder as root
+app.use(express.static('public'))
+
+// Allow front-end access to node_modules folder
+// eslint-disable-next-line node/no-path-concat
+app.use('/scripts', express.static(`${__dirname}/node_modules/`))
+
+// Listen for HTTP requests on port 3000
+app.listen(port, () => {
+    console.log('listening on %d', port)
+})
